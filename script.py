b4881e6a2d93471b99e628646ff8b0d622dd0407
byunjaebin <109408326+byunjaebin@users.noreply.github.com>
2022-07-16 16:41:30 +0900

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ def write(name, desc):
 
 # 하단 버튼 링크 연결 함수
 def button(*args):
-  link = "https://www.naver.com/" # https:// 꼭 붙여야 연결됩니다!
+  link = "https://www.leagueoflegends.com" # https:// 꼭 붙여야 연결됩니다!
   webbrowser.open(link)
 
 # 배경 색깔 설정 함수
@@ -25,15 +25,15 @@ colors = ["green", "blue"]
 background(colors)
 
 # 이름과 설명, 버튼에 들어갈 글 설정
-write("name", "이름")
-write("description", "설명")
-write("button", "버튼")
+write("name", "변재빈")
+write("description", "한울안중학교")
+write("button", "롤")
 
 # 상세설명에 들어갈 제목과 글 설정
 informations = {
-  "제목1": "내용1",
-  "제목2": "내용2",
-  "제목3": "내용3",
-  "제목4": "내용4"
+  "생년월일": "20070630",
+  "좋아하는 것": "게임",
+  "싫어하는 것": "브로콜리",
+  "없음": "없음"
 }
 information(informations)
